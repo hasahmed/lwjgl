ca78771f5b6e93ad7b9910c19206b44f1d38064c
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-10-19 11:04:06 -0400

    Moved Window creation into its own class

--- src/com/shapegame/Game.java
+++ src/com/shapegame/Game.java
@@ -20,12 +20,6 @@ public class Game {
     private int progHandle; //the handle to the shader program
     private int squareVertexBuffer;
     private int trinagleVertexBuffer;
-    private int vertextArrayID;
-
-
-    private final int viewWidth = 800;
-    private final int viewHeight = 600;
-
 
     private float triangleVerts[] = {
             0f, 0.5f, 0f, //lower left,
@@ -33,29 +27,13 @@ public class Game {
             0f, 1f, 0f // left
     };
 
-    private float squareVerts[] = {
-            -0.5f, -0.5f, 0f, //lower left,
-            0.5f, -0.5f, 0f, //lower right
-            -0.5f, 0.5f, 0f, // top left
-
-            -0.5f, 0.5f, 0f, //top left
-            0.5f, -0.5f, 0f, //lower right
-            0.5f, 0.5f, 0f, //top
-    };
-
-    int getViewWidth(){
-        return this.viewWidth;
-    }
-
-    int getViewHeight(){
-        return this.viewHeight;
-    }
-
+    private float squareVerts[];
+    float[] smallSquareVerts;
 
     private void teardown(){
         // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
+        glfwFreeCallbacks(this.window);
+        glfwDestroyWindow(this.window);
 
         // Terminate GLFW and free the error callback
         glfwTerminate();
@@ -70,74 +48,76 @@ public class Game {
 
     }
 
-    float[] smallSquareVerts;
 
     private void createWindow(){
-
+        Window w = new Window(800, 600);
+        this.window = w.getWindow();
+        System.out.println(this.window);
+//
         squareVerts = GLUtil.makeSquare(0, 0, 40);
-
-
-        smallSquareVerts = GLUtil.makeSquare(200, 10, 100);
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( !glfwInit() )
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_SAMPLES, 4); //antialiasing
-        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE); // the window will stay hidden after creation
-//        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will not be resizable
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-        // Create the window
-        window = glfwCreateWindow(viewWidth, viewHeight, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-
-            if (key == GLFW_KEY_A){
-                System.out.println("A");
-            }
-        });
-
-        // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
-        // Make the window visible
-        glfwShowWindow(window);
+//
+//
+//        smallSquareVerts = GLUtil.makeSquare(200, 10, 100);
+//        // Setup an error callback. The default implementation
+//        // will print the error message in System.err.
+//        GLFWErrorCallback.createPrint(System.err).set();
+//
+//        // Initialize GLFW. Most GLFW functions will not work before doing this.
+//        if ( !glfwInit() )
+//            throw new IllegalStateException("Unable to initialize GLFW");
+//
+//        // Configure GLFW
+//        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+//        glfwWindowHint(GLFW_SAMPLES, 4); //antialiasing
+//        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE); // the window will stay hidden after creation
+////        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+//        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will not be resizable
+//
+//        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+//        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//
+//        // Create the window
+//        window = glfwCreateWindow(viewWidth, viewHeight, "Hello World!", NULL, NULL);
+//        if ( window == NULL )
+//            throw new RuntimeException("Failed to create the GLFW window");
+//
+//        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+//        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+//            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+//                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+//
+//            if (key == GLFW_KEY_A){
+//                System.out.println("A");
+//            }
+//        });
+//
+//        // Get the thread stack and push a new frame
+//        try ( MemoryStack stack = stackPush() ) {
+//            IntBuffer pWidth = stack.mallocInt(1); // int*
+//            IntBuffer pHeight = stack.mallocInt(1); // int*
+//
+//            // Get the window size passed to glfwCreateWindow
+//            glfwGetWindowSize(window, pWidth, pHeight);
+//
+//            // Get the resolution of the primary monitor
+//            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+//
+//            // Center the window
+//            glfwSetWindowPos(
+//                    window,
+//                    (vidmode.width() - pWidth.get(0)) / 2,
+//                    (vidmode.height() - pHeight.get(0)) / 2
+//            );
+//        } // the stack frame is popped automatically
+//
+//        // Make the OpenGL context current
+//        glfwMakeContextCurrent(window);
+//        // Enable v-sync
+//        glfwSwapInterval(1);
+//        // Make the window visible
+//        glfwShowWindow(window);
     }
 
     private void init() {
@@ -236,6 +216,7 @@ public class Game {
 
     public static void main(String[] args) {
         new Game().run();
+//        new Game().createWindow();
     }
 
 }
---
+++ src/com/shapegame/Window.java
@@ -0,0 +1,82 @@
+package com.shapegame;
+
+/**
+ * Created by Hasan Y Ahmed on 10/18/17.
+ */
+import org.lwjgl.glfw.*;
+import org.lwjgl.system.*;
+import org.lwjgl.opengl.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+
+public class Window {
+   long window;
+
+   Window(int width, int height){
+      // Setup an error callback. The default implementation
+      // will print the error message in System.err.
+      GLFWErrorCallback.createPrint(System.err).set();
+
+      // Initialize GLFW. Most GLFW functions will not work before doing this.
+      if ( !glfwInit() )
+         throw new IllegalStateException("Unable to initialize GLFW");
+
+      // Configure GLFW
+      glfwDefaultWindowHints(); // optional, the current window hints are already the default
+      glfwWindowHint(GLFW_SAMPLES, 4); //antialiasing
+      glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE); // the window will stay hidden after creation
+//        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will not be resizable
+
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+      // Create the window
+      this.window = glfwCreateWindow(width, height, "Hello World!", NULL, NULL);
+      if ( this.window == NULL )
+         throw new RuntimeException("Failed to create the GLFW window");
+
+      // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+      glfwSetKeyCallback(this.window, (window, key, scancode, action, mods) -> {
+         if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+            glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+      });
+
+      // Get the thread stack and push a new frame
+      try ( MemoryStack stack = stackPush() ) {
+         IntBuffer pWidth = stack.mallocInt(1); // int*
+         IntBuffer pHeight = stack.mallocInt(1); // int*
+
+         // Get the window size passed to glfwCreateWindow
+         glfwGetWindowSize(this.window, pWidth, pHeight);
+
+         // Get the resolution of the primary monitor
+         GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+         // Center the window
+         glfwSetWindowPos(
+                 this.window,
+                 (vidmode.width() - pWidth.get(0)) / 2,
+                 (vidmode.height() - pHeight.get(0)) / 2
+         );
+      } // the stack frame is popped automatically
+
+      // Make the OpenGL context current
+      glfwMakeContextCurrent(this.window);
+      // Enable v-sync
+      glfwSwapInterval(1);
+      // Make the window visible
+      glfwShowWindow(this.window);
+   }
+
+   public long getWindow() {
+      return window;
+   }
+}
